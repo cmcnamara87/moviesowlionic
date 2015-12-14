f69c1066a1c914038630934357a6575b215b7ab4
Craig McNamara <cmcnamara87@gmail.com>
2015-12-14 13:37:01 +1000

    Update 2015-12-14T03:36:58.029Z

--- js/app.js
+++ js/app.js
@@ -5,7 +5,11 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('moviesowlApp', ['ionic', 'config', 'templates'])
+angular.module('moviesowlApp', [
+    'ionic',
+    'config',
+    'templates',
+    'angularMoment'])
 
     .run(function ($ionicPlatform, ENV, $rootScope, autoupdate) {
 
@@ -28,7 +32,10 @@ angular.module('moviesowlApp', ['ionic', 'config', 'templates'])
             }
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
-                StatusBar.styleDefault();
+                StatusBar.styleLightContent();
+            }
+            if (navigator.splashscreen) {
+                navigator.splashscreen.hide();
             }
         });
     })
--- js/cinema-inject.js
+++
@@ -1,7 +0,0 @@
-//$('.continue').get(0).click(function() {
-//    if(!$('#MemberCards').val()) {
-
-setTimeout(function() {$('#MemberCards').val('800012851988');}, 2000);
-
-    //}
-//});
